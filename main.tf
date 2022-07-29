446fe7b197fb3f0c32218f66e3546bdd110f24ea
aswini <aswini@email.com>
2022-07-29 15:22:38 +0530

    added gitignore

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 provider "aws" {
-    access_key = "AKIA2PY2HRC47ECYWR44"
-    secret_key = "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"
+    access_key = "AKIA2PY2HRC43RGI4YXK"
+    secret_key = "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"
     region = "us-east-1"
 }
-resource "aws_instance" "myterraform-ec2" {
-ami = "ami-0ca614dcaddfdd29f"
+resource "aws_instance" "myterraform" {
+ami = "ami-05837ea3e41f3fde3"
 instance_type = "t2.micro"
-security_groups = "mysg"
+#security_groups = ${data.security_group.sg}
+key_name = "mykp"
 }
+
+
